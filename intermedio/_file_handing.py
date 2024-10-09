3a38d2a51e5585608686604bc7809a502b6f0cac
Julian <julimillen@gmail.com>
2024-10-09 11:24:02 -0300

    practica json y csv

--- intermedio/_file_handing.py
+++ intermedio/_file_handing.py
@@ -17,3 +17,49 @@ print(txt_file.readline())
 
 txt_file.close()
 
+## .json file
+
+import json 
+
+json_file = open("intermedio/my_file.json","w+")
+json_test =  {
+    "name":"Julian"
+              ,"surname":"Millen"
+              ,"age":24,
+              "language":"Python"}
+
+json.dump(json_test,json_file, indent = 4)
+
+json_file.close()
+
+with open("intermedio/my_file.json") as my_other_file:
+    for line in my_other_file.readlines():
+        print(line)
+
+
+json_dict = json.load(open("intermedio/my_file.json"))
+print(json_dict)
+print(type(json_dict))
+print(json_dict['name'])
+
+
+# .csv file
+import csv
+csv_file = open("intermedio/my_file.csv","w+")
+
+json_test =  {
+    "name":"Julian"
+              ,"surname":"Millen"
+              ,"age":24,
+              "language":"Python"}
+
+csv_writer = csv.writer(csv_file)
+csv_writer.writerow(["name","surname","age","language"])
+csv_writer.writerow(["Julian","Millen","24","Python"])
+
+json_file.close()
+
+# xlsx file para utilizarlo debe instalarse el modulo
+
+# .xml file
+#import xml
